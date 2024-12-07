c627d345b5fff19dcac7a859e4dff5267c181b65
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2024-12-07 11:49:19 +0600

    added loading state

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -14,19 +14,22 @@ export const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   console.log(user);
-  const [loading, setLoading] = useState();
+  const [loading, setLoading] = useState(true);
 
   const createNewUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
   const userLogin = (email, password) => {
+    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
 
   useEffect(() => {
     const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
+      setLoading(false);
     });
     return () => {
       unSubscribe();
@@ -34,6 +37,7 @@ const AuthProvider = ({ children }) => {
   }, []);
 
   const logOut = () => {
+    setLoading(true);
     toast.success("See you again soon!");
     return signOut(auth);
   };
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -2,11 +2,14 @@ import { useContext } from "react";
 import { AuthContext } from "../provider/AuthProvider";
 import { Navigate } from "react-router-dom";
 import PropTypes from "prop-types";
+import Loading from "../pages/Loading";
 
 const PrivateRoute = ({ children }) => {
   console.log(children);
-  const { user } = useContext(AuthContext);
-
+  const { user, loading } = useContext(AuthContext);
+  if (loading) {
+    return <Loading></Loading>;
+  }
   if (user && user?.email) {
     return children;
   }

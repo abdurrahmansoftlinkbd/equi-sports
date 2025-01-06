017f48f72fa57c4a572e3bfd212bace407fc440e
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2025-01-06 19:19:33 +0600

    slider modified

---
+++ src/components/SwiperSlider/Slide.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+const Slide = ({ image, text, desc, btn }) => {
+  return (
+    <div
+      className="w-full bg-center bg-cover h-[38rem]"
+      style={{
+        backgroundImage: `url(${image})`,
+      }}
+    >
+      <div className="relative w-full h-full bg-gray-900/70">
+        <div className="w-4/5 absolute top-[20%] left-16">
+          <h2 className="text-3xl lg:text-6xl font-poppins font-semibold text-white">
+            {text}
+          </h2>
+          <p className="w-4/5 font-roboto text-white mt-4 text-xl">{desc}</p>
+          {btn && (
+            <Link
+              to="/allSportsEquipment"
+              className="btn font-inter bg-primary border-primary text-white hover:bg-light hover:border-light mt-6"
+            >
+              {btn}
+            </Link>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Slide.propTypes = {
+  image: PropTypes.string,
+  text: PropTypes.string,
+  desc: PropTypes.string,
+  btn: PropTypes.string,
+};
+
+export default Slide;
--- src/components/SwiperSlider/SwiperSlider.jsx
+++ src/components/SwiperSlider/SwiperSlider.jsx
@@ -1,84 +1,50 @@
-// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-
-import "./swiperSlider.css";
-
-// import required modules
-import { Pagination, Navigation } from "swiper/modules";
-import { Link } from "react-router-dom";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import Slide from "./Slide";
 
 const SwiperSlider = () => {
-  const slides = [
-    {
-      image:
-        "https://i.ibb.co.com/q5XJ4fV/kenny-nguy-n-J97-Bz-KSPZo-U-unsplash.jpg",
-      title: "Elevate Your Game with Premium Sports Equipment",
-      paragraph:
-        "Discover the ultimate collection of high-quality sports gear and accessories tailored to enhance your performance. Whether you are a seasoned athlete or just starting, our curated range caters to all your sporting needs with unbeatable durability and style.",
-      button: "Get Started",
-    },
-    {
-      image: "https://i.ibb.co.com/NsP3x1y/pexels-steshkawillems-1308713.jpg",
-      title: "Unleash Your Potential with Every Game",
-      paragraph:
-        "From precision gear to performance apparel, our collection is designed to push you further. Find everything you need to perform at your best, whether you're on the field, court, or track. With top brands and cutting-edge designs, we’re here to help you achieve greatness.",
-    },
-    {
-      image:
-        "https://i.ibb.co.com/PFJxPxT/cristina-anne-costello-QZKFRL-HBUw-unsplash.jpg",
-      title: "Gear Up for Success with Cutting Edge Innovation",
-      paragraph:
-        "Stay ahead of the competition with our innovative sports equipment. From high-tech training tools to durable gear, our products are designed to elevate your performance. Explore our range and gear up for the next level in your athletic journey.",
-    },
-  ];
-
   return (
     <div>
       <Swiper
-        pagination={{}}
-        navigation={true}
-        modules={[Pagination, Navigation]}
-        className="mySwiper"
         spaceBetween={30}
-        slidesPerView={1}
-        autoplay={{ delay: 3000 }}
+        centeredSlides={true}
         loop={true}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
       >
-        {slides.map((slide, index) => (
-          <SwiperSlide key={index}>
-            <div className="relative ">
-              <img
-                src={slide.image}
-                alt=""
-                className="w-full h-full object-cover"
-              />
-
-              <div className="absolute inset-0 bg-gradient-to-r from-black via-gray-900 to-transparent opacity-70"></div>
-
-              <h1 className="absolute inset-0 top-36 left-14 text-white w-2/3 text-5xl font-bold">
-                {slide.title}
-              </h1>
-              <p className="absolute inset-0 top-[17rem] left-14 w-3/5 text-white  ">
-                {slide.paragraph}
-              </p>
-              {slide?.button ? (
-                <Link
-                  to="/allSportsEquipment"
-                  className="absolute inset-0 top-[42%] left-14 w-40 bg-primary border-primary hover:bg-light hover:border-light text-white btn"
-                >
-                  {slide?.button}
-                </Link>
-              ) : (
-                ""
-              )}
-            </div>
-          </SwiperSlide>
-        ))}
+        <SwiperSlide>
+          <Slide
+            image="https://i.ibb.co.com/q5XJ4fV/kenny-nguy-n-J97-Bz-KSPZo-U-unsplash.jpg"
+            text="Elevate Your Game with Premium Sports Equipment"
+            desc="Discover the ultimate collection of high-quality sports gear and accessories tailored to enhance your performance. Whether you are a seasoned athlete or just starting, our curated range caters to all your sporting needs with unbeatable durability and style."
+            btn="Get Started"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Slide
+            image="https://i.ibb.co.com/NsP3x1y/pexels-steshkawillems-1308713.jpg"
+            text="Unleash Your Potential with Every Game"
+            desc="From precision gear to performance apparel, our collection is designed to push you further. Find everything you need to perform at your best, whether you're on the field, court, or track. With top brands and cutting-edge designs, we’re here to help you achieve greatness."
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Slide
+            image="https://i.ibb.co.com/PFJxPxT/cristina-anne-costello-QZKFRL-HBUw-unsplash.jpg"
+            text="Gear Up for Success with Cutting Edge Innovation"
+            desc="Stay ahead of the competition with our innovative sports equipment. From high-tech training tools to durable gear, our products are designed to elevate your performance. Explore our range and gear up for the next level in your athletic journey."
+          />
+        </SwiperSlide>
       </Swiper>
     </div>
   );

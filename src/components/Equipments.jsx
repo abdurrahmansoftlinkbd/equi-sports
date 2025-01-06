08d8a4498c7f8164ce36b98c7131af3a9f96bcc5
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2025-01-06 23:42:30 +0600

    equipments section modified

--- src/components/Equipments.jsx
+++ src/components/Equipments.jsx
@@ -1,16 +1,26 @@
 import PropTypes from "prop-types";
 import Equipment from "./Equipment";
 import { Fade } from "react-awesome-reveal";
+import { Link } from "react-router-dom";
+import { FaExternalLinkAlt } from "react-icons/fa";
 
 const Equipments = ({ equipments }) => {
   return (
     <Fade triggerOnce={true}>
-      <div className="grid justify-items-center md:gap-x-7 gap-y-7 mb-24 grid-cols-1 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+      <div className="font-poppins grid justify-items-center mb-12 md:gap-x-7 gap-y-7 grid-cols-1 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
         {equipments
           .map((equipment) => (
             <Equipment key={equipment._id} equipment={equipment}></Equipment>
           ))
           .slice(0, 6)}
+      </div>
+      <div className="text-center mb-24">
+        <Link
+          to="/allSportsEquipment"
+          className="btn btn-md text-white bg-primary border-primary hover:bg-light hover:border-light"
+        >
+          Show All Sports Equipments <FaExternalLinkAlt />
+        </Link>
       </div>
     </Fade>
   );
